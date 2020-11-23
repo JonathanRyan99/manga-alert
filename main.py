438d383756e5f2370cabb7440475edcc0e692a71
Jonathan Ryan <jonny788@live.co.uk>
2020-11-23 19:04:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+#this program is made specifically this website as of 23/11/2020 
+#https://w20.readheroacademia.com/
+
+from bs4 import BeautifulSoup
+import requests
+import webbrowser
+
+
+
+
+def pageAvaliable(chapter):
+    url = "https://w20.readheroacademia.com/manga/boku-no-hero-academia-chapter-" + str(chapter) + "/"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+
+    #looks for chapter title to confirm valid page
+    try:
+        title = soup.find("h1",{"class": "entry-title"})
+        print(title.text)
+        page = True
+    except:
+        page = False
+
+
+    #looks for upload timer, if present then the chapter has not been released
+    try:
+        countdown = soup.find("div",{"class": "entry-content"})
+        countdown = countdown.h1.strong
+        avalible = False
+    except:
+        avalible = True
+
+
+    return page,avalible, url
+
+def logCheck():
+    try:
+        f = open("log.txt","r")
+        chapter = f.read()
+        f.close()
+    except:
+        print("file not found")
+        f = open("log.txt","w")
+        chapter = input("ENTER CHAPTER NUMBER: ")
+        f.write(chapter)
+        f.close()
+    
+    return chapter
+
+def logSave(chapter):
+    chapter = int(chapter)
+    newChapter = chapter + 1
+    newChapter = str(newChapter)
+     
+    f = open("log.txt" ,"w")
+    f.write(newChapter)
+    f.close()
+
+    
+
+chapter = logCheck()
+print("chapter: ",chapter)
+
+page, avalible, url = pageAvaliable(chapter)
+
+if page & avalible == True:
+    webbrowser.open_new_tab(url)
+    logSave(chapter)
+else:
+    print("chapter: not avaliable")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
