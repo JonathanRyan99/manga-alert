d6af7d4c8d9d2f38af1465c86837ee61e47c4442
Jonathan Ryan <jonny788@live.co.uk>
2020-12-02 11:47:15 +0000

    added csv dictread from log file

--- log_control.py
+++ log_control.py
@@ -1,18 +1,30 @@
 
 import csv
 
-with open('log.csv', 'w', newline='') as csvfile:
-    fieldnames = ['title', 'url', 'chapter']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    writer.writeheader()
-    writer.writerow({'title': 'academia', 'url': 'https://w20.readheroacademia.com/manga/boku-no-hero-academia-chapter-', 'chapter':'22'})
-    writer.writerow({'title': 'Gokushufudou', 'url': 'https://manganelo.com/manga/ya23ux2738298723', 'chapter':'22'})
-    
+def writeToLog(log_contents):
+    with open('log.csv', 'w', newline='') as csvfile:
+        fieldnames = ['title', 'url', 'chapter']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-with open('log.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row['title'], row['url'])
+        writer.writeheader()
+        writer.writerow({'title': 'academia', 'url': 'https://w20.readheroacademia.com/manga/boku-no-hero-academia-chapter-', 'chapter':'22'})
+        writer.writerow({'title': 'Gokushufudou', 'url': 'https://manganelo.com/manga/ya23ux2738298723', 'chapter':'22'})
+
+
+def readFromLog():
+    log_contents = []
+    with open('log.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            log_contents.append(row)
+        return log_contents
+        
+
+
+   
+# for i in range(len(log_contents)):
+#     print(log_contents[i]['title'])
+
 
    
